13cc90e9000471dfdbac41d0c36c52f7e7ee9008
Lukas Fryc <lukas.fryc@exponea.com>
2018-06-25 09:09:57 +0200

    button clickable from React app

--- src/app1/root.component.js
+++ src/app1/root.component.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
 export default class Root extends React.Component {
+
+  buttonLabel;
+
+  constructor() {
+    super();
+    this.buttonLabel = 'Click me';
+  }
+
+  buttonClicked() {
+    this.buttonLabel = 'Hey, I got clicked, wohoo!';
+    setTimeout(() => {
+      alert('Button clicked from React');
+    });
+  }
+
+
   render() {
     return (
       <div style={{marginTop: '100px'}}>
         This was rendered by app 1, which is written in React.
+        <my-button label={this.buttonLabel} onClick={this.buttonClicked.bind(this)}></my-button>
       </div>
     );
   }
